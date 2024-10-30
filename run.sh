bfd22a72a93045ae47b9caa0ebab313580873792
heapops <heapbin1@gmail.com>
2024-10-30 13:57:09 +0800

    add tld-!cn
    Signed-off-by: heapops <heapbin1@gmail.com>

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 #
 
-wget https://github.com/MetaCubeX/geo/releases/download/v1.1/geo-linux-amd64
-wget https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat
-chmod +x geo-linux-amd64
+if [[ ! -f geo-linux-amd64 ]]; then
+    wget https://github.com/MetaCubeX/geo/releases/download/v1.1/geo-linux-amd64
+    chmod +x geo-linux-amd64
+fi
 
-mkdir output && ./geo-linux-amd64 unpack site ./geosite.dat -d output -c 'geolocation-!cn'
+rm -f geosite.dat
+wget https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat
+
+rm -rf output && mkdir output && \
+    ./geo-linux-amd64 unpack site ./geosite.dat -d output -c 'geolocation-!cn' &&
+    ./geo-linux-amd64 unpack site ./geosite.dat -d output -c 'tld-!cn'
 
 cat > .proxy.txt <<EOF
 [AutoProxy 0.2.9]
@@ -15,4 +21,5 @@ cat > .proxy.txt <<EOF
 EOF
 
 grep -E '(^domain:|^full:)' output/geolocation-!cn | sed -e 's/^domain://' -e 's/^full://' -e 's/^/||/' >> .proxy.txt
+grep -E '(^domain:|^full:)' output/tld-!cn | sed -e 's/^domain://' -e 's/^full://' -e 's/^/||/' >> .proxy.txt
 base64 .proxy.txt > proxy.txt
